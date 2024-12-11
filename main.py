eb8b55d117be1e07b86fe2218cfab3a23e6285ca
RainsN <139716821+RainsXe@users.noreply.github.com.>
2024-12-11 23:05:33 +0900

    slash

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import discord
+from discord import app_commands
 import random
 import os
 
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
 
 @client.event  # 起動時
 async def on_ready():
     print('ログインしました')
+
+    # アクティビティを設定
+    new_activity = f"テスト"
+    await client.change_presence(activity=discord.Game(new_activity))
+
+    # スラッシュコマンドを同期
+    await tree.sync()
+
+@tree.command(name='hello', description='Say hello to the world!')
+async def test(interaction: discord.Interaction):
+    await interaction.response.send_message('Hello, World!')
 
 @client.event  # メッセージ受信時
 async def on_message(message):
