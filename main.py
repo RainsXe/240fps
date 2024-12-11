a4281d5d7ee2e7220f5519c2995820790039826d
amano <takuya.amano42@gmail.co>
2024-12-11 11:01:07 +0900

    add ping機能

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 import os
 import random
-from os.path import join, dirname, defpath
+from os.path import join, dirname
 from dotenv import load_dotenv
 
 client = discord.Client(intents=discord.Intents.default())
@@ -10,15 +10,27 @@ load_dotenv(verbose=True)
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-@client.event
+@client.event #起動時
 async def on_ready():
     print('ログインしました')
 
-@client.event
+@client.event #占い
 async def on_message(message):
     if message.content == "!占い":
         unsei = ["大吉", "中吉", "吉", "末吉", "凶", "大凶"]
         choice = random.choice(unsei)
         await message.channel.send(choice)
+        
+@client.event #ping
+async def on_message(message):
+    if message.content == "!ping":
+        #ping値を秒単位で取得
+        raw_ping = client.latency
+        
+        #ミリ秒に変換まるめ
+        raw_ping = round(raw_ping * 1000)
+        
+        #送信
+        await message.reply(f"Pong!\n{raw_ping}ms")
 
 client.run(os.getenv('TOKEN'))
