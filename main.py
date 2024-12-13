cdb30da306f52f0e4b6b10c462f2ed23ace84ddf
amano <takuya.amano42@gmail.co>
2024-12-13 10:35:41 +0900

    slashコマンド使えるように

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
+from tkinter.ttk import Treeview
+
 import discord
-from discord import app_commands
 import random
 import os
+
+from dotenv import load_dotenv
+from discord import app_commands
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
+
 @client.event  # 起動時
 async def on_ready():
     print('ログインしました')
-
-    # スラッシュコマンドを同期
+    
+    #スラッシュコマンド同期
     await tree.sync()
 
-@tree.command(name='hello', description='Say hello to the world!')
-async def test(interaction: discord.Interaction):
-    await interaction.response.send_message('Hello, World!')
+@tree.command(name="test",description="テストコマンドです。")
+async def test_command(interaction: discord.Interaction,text:str="てすと"):#デフォルト値を指定
+    await interaction.response.send_message(text,ephemeral=True)
 
 @client.event  # メッセージ受信時
 async def on_message(message):
@@ -36,17 +42,9 @@ async def on_message(message):
         raw_ping = round(client.latency * 1000)  # ミリ秒に変換
         await message.reply(f"Pong!\n{raw_ping}ms")
 
-    elif message.content == "!RandAtkOpe":
-        atkOpe = ["STRIKER", "DEMOS", "RAM", "BRAVA", "GRIM", "SENS", "OSA", "FLORES", "ZERO", "ACE", "IANA", "KALI", "AMARU",
-                  "NOKK", "GRIDLOCK", "NOMAD", "MAVERICK","LION", "FINKA", "DOKKAEBI", "ZOFIA", "YING", "JACKAL", "HIBANA",
-                  "CAPITAO", "BLACKBEARD", "BUCK", "SLEDGE", "THATCHER", "ASH", "THERMITE", "MONTAGNE", "TWITCH", "BLITZ",
-                  "IQ", "FUZE", "GLAZ"]
-
-        choice = random.choice(atkOpe)
-        await message.channel.send(choice)
-
+load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 if TOKEN:
     client.run(TOKEN)
 else:
-    print("Tokenが見つかりませんでした")
+    print("TOKENが見つかりませんでした。")
